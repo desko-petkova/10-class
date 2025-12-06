1a1d061af0a6d3577efb76ae40b2966b4c875fc5
desko-petkova <desko.petkova@gmail.com>
2025-12-06 15:20:38 +0200

    Сортиране на списъци

---
+++ 02. Списъци/Решения/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿namespace BubbleSort
+{
+    //Алгоритъм за сортиране - метод на мехурчето
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < nums.Count-1; i++)
+            {
+                for (int j = 0; j < nums.Count - 1 - i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+   
+            Console.WriteLine(string.Join(", ", nums));
+
+        }
+    }
+}
---
+++ 02. Списъци/Решения/Built_Method_Sort_Reverse_Min_Max.cs
@@ -0,0 +1,31 @@
+﻿namespace SortNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+           List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            // Вграден метод за сортиране по възходящ ред (от най-малко към най-голямо)
+            nums.Sort();
+            Console.WriteLine("Sorted: ");
+            Console.WriteLine(string.Join(", ", nums));
+
+            // Вграден метод, който обръща реда на елементите в списъка
+            nums.Reverse();
+            Console.WriteLine("Reversed: ");
+            Console.WriteLine(string.Join(", ", nums));
+
+            // Намираме минималната стойност в списъка чрез вградения метод Min()
+            int min = nums.Min();
+            // Намираме максималната стойност чрез вградения метод Max()
+            int max = nums.Max();
+            Console.WriteLine("Max and Min value:");
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+        }
+    }
+}
